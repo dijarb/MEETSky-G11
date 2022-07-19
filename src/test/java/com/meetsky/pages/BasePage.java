e9d1997211a17fa81d876e64fd7f13c9e0db2b97
Abid <i_learn_tek@outlook.com>
2022-07-19 21:11:40 +0300

    Notes Page  created

--- src/test/java/com/meetsky/pages/BasePage.java
+++ src/test/java/com/meetsky/pages/BasePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public class  BasePage {
 
     @FindBy(id = "nextcloud")
     public WebElement homePageLink;
