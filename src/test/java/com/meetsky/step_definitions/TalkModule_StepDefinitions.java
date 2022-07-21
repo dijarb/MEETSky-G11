2bd3adb0ef9d45965b99a41accbc41398687b85a
tuncherdurmush <tuncherdurmush@gmail.com>
2022-07-21 12:45:38 +0100

    First Scenario is Created

---
+++ src/test/java/com/meetsky/step_definitions/TalkModule_StepDefinitions.java
@@ -0,0 +1,42 @@
+package com.meetsky.step_definitions;
+
+import com.meetsky.pages.LoginPage;
+import com.meetsky.utilities.BrowserUtils;
+import com.sun.jna.Library;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TalkModule_StepDefinitions {
+    @When("User clicks talk icon on header menu")
+    public void user_clicks_talk_icon_on_header_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks plus icon from opened menu on left side of the page")
+    public void user_clicks_plus_icon_from_opened_menu_on_left_side_of_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User types the group name in input box")
+    public void user_types_the_group_name_in_input_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks Add participants button on the bottom right")
+    public void user_clicks_add_participants_button_on_the_bottom_right() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User selects any of the contacts in contact list")
+    public void user_selects_any_of_the_contacts_in_contact_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("New group conversation is created and displayed with the same name on the top left side")
+    public void new_group_conversation_is_created_and_displayed_with_the_same_name_on_the_top_left_side() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
