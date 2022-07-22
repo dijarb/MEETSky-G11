52cca5f42136eed5c262729eb430677e95960438
cihangir <cihangiracikgoz01@gmail.com>
2022-07-22 12:19:22 +0100

    adding functionality first ac done

--- src/test/java/com/meetsky/step_definitions/Adding_StepDefinitions.java
+++ src/test/java/com/meetsky/step_definitions/Adding_StepDefinitions.java
@@ -1,15 +1,31 @@
 package com.meetsky.step_definitions;
 
-import com.meetsky.pages.LoginPage;
-import io.cucumber.java.en.Given;
+import com.meetsky.pages.AddingPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class Adding_StepDefinitions {
 
-    LoginPage loginPage = new LoginPage();
+    AddingPage addingPage = new AddingPage();
 
-    @Given("user is on main page")
-    public void user_is_on_main_page() {
-        loginPage.login();
+    @When("user clicks on three dots button")
+    public void user_clicks_on_three_dots_button(){
+        addingPage.threeDots.click();
     }
 
+    @And("user clicks Add to favorites button")
+    public void userClicksAddToFavoritesButton() {
+        addingPage.addToFavorites.click();
+    }
+
+    @And("user clicks to Favorites")
+    public void userClicksToFavorites() {
+        addingPage.favoritesFile.click();
+    }
+
+    @Then("user should see his favorite files in Favorite part")
+    public void userShouldSeeHisFavoriteFilesInFavoritePart() {
+        addingPage.seenInFavorites.isDisplayed();
+    }
 }
