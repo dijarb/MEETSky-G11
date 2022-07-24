305b8ba0c521faca5ea04daa995deeefed53eca2
tuncherdurmush <tuncherdurmush@gmail.com>
2022-07-24 16:59:42 +0100

    First Scenario work done

--- src/test/java/com/meetsky/step_definitions/TalkModule_StepDefinitions.java
+++ src/test/java/com/meetsky/step_definitions/TalkModule_StepDefinitions.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.jsoup.Connection;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,42 +20,45 @@ public class TalkModule_StepDefinitions {
     public void user_clicks_talk_icon_on_header_menu() {
 
         BasePage basePage = new BasePage();
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),60);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 60);
         wait.until(ExpectedConditions.visibilityOf(basePage.spreedPageLink));
         basePage.spreedPageLink.click();
 
 
-
     }
+
     @When("User clicks plus icon from opened menu on left side of the page")
     public void user_clicks_plus_icon_from_opened_menu_on_left_side_of_the_page() throws InterruptedException {
         WebElement plusIcon = Driver.getDriver().findElement(By.xpath("//button[@slot='trigger']"));
-        BrowserUtils.waitForClickablility(plusIcon,30);
+        BrowserUtils.waitForClickablility(plusIcon, 30);
         plusIcon.click();
-        BrowserUtils.waitFor(5);
+        BrowserUtils.waitFor(3);
 
 
     }
+
     @When("User types the group name in input box")
     public void user_types_the_group_name_in_input_box() {
         WebElement textField = Driver.getDriver().findElement(By.xpath("//input[@class='conversation-name']"));
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),30);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 30);
         wait.until(ExpectedConditions.visibilityOf(textField));
         textField.sendKeys("Friends");
 
     }
+
     @When("User clicks Add participants button on the bottom right")
     public void user_clicks_add_participants_button_on_the_bottom_right() {
-       WebElement addButton = Driver.getDriver().findElement(By.xpath("//button[@class='navigation__button navigation__button-right primary']"));
-        BrowserUtils.waitForClickablility(addButton,30);
-       addButton.click();
-        BrowserUtils.waitFor(3);
+        WebElement addButton = Driver.getDriver().findElement(By.xpath("//button[@class='navigation__button navigation__button-right primary']"));
+        BrowserUtils.waitForClickablility(addButton, 30);
+        addButton.click();
+        BrowserUtils.waitFor(2);
 
     }
+
     @When("User selects any of the contacts in contact list")
     public void user_selects_any_of_the_contacts_in_contact_list() {
-       WebElement select1 = Driver.getDriver().findElement(By.xpath("(//span[@class='participant-row__user-name'])[2]"));
-       select1.click();
+        WebElement select1 = Driver.getDriver().findElement(By.xpath("(//span[@class='participant-row__user-name'])[2]"));
+        select1.click();
         BrowserUtils.waitFor(2);
 
         WebElement select2 = Driver.getDriver().findElement(By.xpath("(//span[@class='participant-row__user-name'])[3]"));
@@ -71,10 +75,11 @@ public class TalkModule_StepDefinitions {
 
 
     }
+
     @Then("New group conversation is created and displayed with the same name on the top left side")
     public void new_group_conversation_is_created_and_displayed_with_the_same_name_on_the_top_left_side() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement groupName = Driver.getDriver().findElement(By.xpath("acli__content__line-one__title"));
+        Assert.assertTrue(groupName.isDisplayed());
     }
 
 }
