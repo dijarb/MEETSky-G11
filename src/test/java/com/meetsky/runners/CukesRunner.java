595e82512ffd312e6f42a0519cf2fa3ed5dfa9e2
Joshua <98660305+JoshuaOsin@users.noreply.github.com>
2022-07-25 18:07:54 -0400

    METSK-514 completed

--- src/test/java/com/meetsky/runners/CukesRunner.java
+++ src/test/java/com/meetsky/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features"},
         glue = {"com/meetsky/step_definitions"},
         dryRun = false,
-        tags = "@abid"
+        tags = ""
 )
 public class CukesRunner {
 }
