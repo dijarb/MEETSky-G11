20a79e8800b0cbbe0e382589dc00789a8cb25651
Bedirhan Ceylan <68332000+bedircyln@users.noreply.github.com>
2022-07-25 17:48:48 +0100

    commit

--- src/test/java/com/meetsky/runners/CukesRunner.java
+++ src/test/java/com/meetsky/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features/contactsModuleFunctionality.feature"},
         glue = {"com/meetsky/step_definitions"},
         dryRun = false,
-        tags = "@789"
+        tags = ""
 )
 public class CukesRunner {
 }
