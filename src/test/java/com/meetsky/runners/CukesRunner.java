511c3fc3e0a1d8a401875339fbe2e8e787250258
Dijar <dijar.bucinca@mikrobiz-ks.com>
2022-07-25 15:00:52 +0200

    CukesRunner back to default

--- src/test/java/com/meetsky/runners/CukesRunner.java
+++ src/test/java/com/meetsky/runners/CukesRunner.java
@@ -12,10 +12,10 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
-        features = {"src/test/resources/features/talkModule.feature"},
+        features = {"src/test/resources/features"},
         glue = {"com/meetsky/step_definitions"},
         dryRun = false,
-        tags = "@789"
+        tags = ""
 )
 public class CukesRunner {
 }
